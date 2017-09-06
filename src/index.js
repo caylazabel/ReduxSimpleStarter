c406485e12369160e49c13d6c61353d9cb114385
caylazabel <caylazabel@gmail.com>
2017-09-06 16:12:21 -0400

    added index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // create component. this component// //should produce html.//
 
+const App = function() {
+  return <div>Hi!</div>
+}
+
 //take this compoent's generated HTML and //put it on the page//
