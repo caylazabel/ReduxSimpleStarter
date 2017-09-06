2cb48193a6e023fe3bf1ee2f2d556b3e71f78424
caylazabel <caylazabel@gmail.com>
2017-09-06 16:21:29 -0400

    showing error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // create component. this component// //should produce html.//
 
 const App = function() {
-  return <div>Hi!</div>
+  return <div>Hi!</div>;
 }
 
 //take this compoent's generated HTML and //put it on the page//
+
+React.render(App);
