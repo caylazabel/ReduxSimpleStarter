e26eaf0332431d348d77a0b5fdc3b63c79dd1b0c
caylazabel <caylazabel@gmail.com>
2017-09-06 16:35:56 -0400

    finally renders

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 // create component. this component// //should produce html.//
 
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
 //take this compoent's generated HTML and //put it on the page//
 
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
